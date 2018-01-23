d318b12b9b112143ef16edfec6979c77cb711679
Repain Paul <paul.repain@yahoo.com>
2018-01-23 15:34:45 +0100

    nouv. méthode : estVivant (return bool)

--- Heros.java
+++ Heros.java
@@ -4,15 +4,50 @@ public class Heros
     {
     private String nom ;
     private String classe ;
+    private char genre ;
+    private int pointsDeVie = 100 ;
+    private int attaque = 10 ;
+    private int defense = 10 ;
+    private int dommages = 10 ;
 
-    public Heros(String parNom, String parClasse)
+    public Heros(String parNom, String parClasse, char parGenre)
         {
         nom = parNom ;
         classe = parClasse ;
+        genre = parGenre ;
+        }
+
+    public Heros(int pv, int atk, int def, int domm)
+        {
+        pointsDeVie = pv ;
+        attaque = atk ;
+        defense = def ;
+        dommages = domm ;
+        }
+
+    public String toString()
+        {
+        if (genre=='f')
+            return nom+" la "+classe ;
+        else
+            return nom+" le "+classe ;
+        }
+
+    public boolean estVivant()
+        {
+        return pointsDeVie>0 ;
         }
 
     public static void main(String []args)
         {
-        Heros Bob = new Heros("Bob", "Magicien") ;
+        Heros Bob = new Heros("Bob", "Magicien", 'm') ;
+        System.out.println(Bob);// inutile d'ajouter .toString
+        // println prend automatiquement la methode toString
+
+        Heros Zoe = new Heros("Zoe", "Barbare", 'f') ;
+        System.out.println(Zoe);
+
+        System.out.println(Zoe.estVivant());
+
         }
     }
