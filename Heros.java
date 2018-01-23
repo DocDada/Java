7145dd0788d59d4a9a3e682df1126cfb3f1f7384
Repain Paul <paul.repain@yahoo.com>
2018-01-23 15:01:42 +0100

    creation d'une classe Heros basique

---
+++ Heros.java
@@ -0,0 +1,18 @@
+// TP GIT
+//
+public class Heros
+    {
+    private String nom ;
+    private String classe ;
+
+    public Heros(String parNom, String parClasse)
+        {
+        nom = parNom ;
+        classe = parClasse ;
+        }
+
+    public static void main(String []args)
+        {
+        Heros Bob = new Heros("Bob", "Magicien") ;
+        }
+    }
