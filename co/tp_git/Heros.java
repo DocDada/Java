5595d67aae04829d7aebd4a1b24bb2cf8f2edfc4
Poulpy <paul.repain@yahoo.com>
2018-01-25 23:19:18 +0100

    tp git fini; + ou - propre

--- co/tp_git/Heros.java
+++ co/tp_git/Heros.java
@@ -1,5 +1,7 @@
 // TP GIT
-//
+import java.util.Scanner ;
+
+
 public class Heros
     {
     private String nom ;
@@ -24,7 +26,7 @@ public class Heros
         attaque = atk ;
         defense = def ;
         dommages = domm ;
-        }
+        }// Heros
 
     public String toString()
         {
@@ -32,45 +34,114 @@ public class Heros
             return nom+" la "+classe ;
         else
             return nom+" le "+classe ;
-        }
+        }// toString
 
     public boolean estVivant()
         {
         return pointsDeVie>0 ;
-        }
-    
-    public String Soigner()
+        }// estVivant
+
+    public void Soigner()
         {
         this.pointsDeVie = pointsDeVieMax ;
-        return this.toString()+" a regagné tous ses points de vie !" ;
-        }
+        System.out.println(this.toString()+" a regagné tous ses points de vie !");
+        }// Soigner
 
-    public String Attaquer(Heros defens)
+    public void Attaquer(Heros defens)
+        {
+        Scanner sc = new Scanner(System.in);
+        int choix ;
+        switch (this.classe)
+            {
+            case "Barbare":
+                System.out.println("--> Attaque normale\n--> Frappe éclair\n--> Frappe de l'ours") ;
+                //Scanner sc = new Scanner(System.in);
+                choix = sc.nextInt() ;
+                switch (choix)
+                    {
+                    case 1:
+                        this.AttaqueSpe(defens, 0, 0, "un coup d'épée") ;
+                        return ;
+                    case 2 :
+                        this.AttaqueSpe(defens, 0.1f, -1, "un coup fulgurant") ;
+                        return ;
+                    case 3:
+                        this.AttaqueSpe(defens, -0.2f, +5, "l'attaque de l'ours") ;
+                        return ;
+                    }
+            case "Magicien":
+                System.out.println("--> Attaque normale\n--> Boule de feu\n--> Cyclone de Turchin\n--> Soin") ;
+                choix = sc.nextInt() ;
+                switch (choix)
+                    {
+                    case 1:
+                        this.AttaqueSpe(defens, 0, 0, "un petit coup de baton") ;
+                        return ;
+                    case 2 :
+                        this.AttaqueSpe(defens, -0.1f, 1, "une boule incandescente, s'abattant") ;
+                        return ;
+                    case 3:
+                        this.AttaqueSpe(defens, 0.2f, +5, "la puissance d'un cyclone") ;
+                        return ;
+                    case 4:
+                        this.Soigner() ;
+                        return ;
+                    }
+            }
+        }// Attaquer
+
+    public void AttaqueSpe(Heros defens, float factProba, int factAttaq, String nomAttaq)
         {
         double proba = Math.random() ;
-        if (proba > (float) (this.attaque / (defens.attaque+defens.defense)))
+        float prob=((float)(attaque)/(float)(defens.attaque+defens.defense));
+        if (proba < prob+factProba)
             {
-            defens.pointsDeVie -= this.dommages ;
-            return this.toString()+" attaque "+defens.toString()+" et lui inflige "+this.dommages+" points de dommages !" ;
+            this.dommages = this.DommagesAleatoire() ;
+            defens.pointsDeVie -= this.dommages + factAttaq ;
+            System.out.println(this.toString()+" envoie "+nomAttaq+" sur "+defens.toString()+" et lui inflige "+this.dommages+" points de dommages !");
             }
-        return this.toString()+" attaque "+defens.toString()+" et rate !" ;
-        }
-    
-    public String Combat(Heros defen)
+        else
+            System.out.println(this.toString()+" effectue "+nomAttaq+" sur "+defens.toString()+" ... et rate.");
+        }// AttaqueSpe
+
+    public int DommagesAleatoire()
         {
-        int compteur = 0 ;
-        for ( ; this.pointsDeVie>0 && defen.pointsDeVie>0 ; compteur++)
+        return this.attaque + (int)(Math.random()*10) ;
+        }// DommagesAleatoire
+
+    public void AffichePV(Heros defens)
+        {
+        int i = 0 ;
+        for ( ; i<55 ; i++)
+            System.out.print("_") ;
+        System.out.println("") ;
+        System.out.println("\n>>> "+this.toString()+" ||  POINTS DE VIE : "+this.pointsDeVie+"/"+this.pointsDeVieMax+"\t<<<") ;
+        System.out.println("\n--- "+defens.toString()+" || POINTS DE VIE : "+defens.pointsDeVie+"/"+defens.pointsDeVieMax+"\t---") ;
+        for (i = 0 ; i<55 ; i++)
+            System.out.print("_") ;
+        System.out.println("\n") ;
+        }// Affiche PV
+
+    public void Combat(Heros defens)
+        {
+        for (int compteur = 0 ; this.pointsDeVie>0 && defens.pointsDeVie>0 ; compteur++)
             {
-            if (compteur%2==0)
-                System.out.println(this.Attaquer(defen)) ;
+            if (compteur%2 == 0)
+                {
+                this.AffichePV(defens) ;
+                this.Attaquer(defens) ;
+                }
             else
-                System.out.println(defen.Attaquer(this)) ;
+                {
+                defens.AffichePV(this) ;
+                defens.Attaquer(this) ;
+                }
             }
         if (this.pointsDeVie<=0)
-            return this.toString()+" est mort." ;
+            System.out.println(this.toString()+" est mort.") ;
         else
-            return defen.toString()+" est mort." ;
-        }
+            System.out.println(defens.toString()+" est mort.") ;
+        }// Combat
 
     public static void main(String []args)
         {
@@ -80,7 +151,7 @@ public class Heros
 
         Heros Zoe = new Heros("Zoe", "Barbare", 'f') ;
         System.out.println(Zoe);
-        System.out.println(Bob.Attaquer(Zoe));
-        System.out.println(Bob.Combat(Zoe));
-        }
+
+        Bob.Combat(Zoe);
+        }// main
     }
