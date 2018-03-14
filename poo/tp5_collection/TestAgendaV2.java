33f69ac9d695d30360db87ef6ba54309bbcc596c
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2018-03-14 17:33:48 +0100

    Fini TP5 Collection

--- poo/tp5_collection/TestAgendaV2.java
+++ poo/tp5_collection/TestAgendaV2.java
@@ -12,9 +12,11 @@ public class TestAgendaV2 {
 		try {
 			AgendaV2 agd = new AgendaV2();
 			agd.ajout(new Evenement(new Date(2,1,1999), "Titre", "Lieu"));
+			agd.ajout(new Evenement(new Date(4,7,2017), "Comédie", "Paris"));
+			agd.ajout(new Evenement(new Date(12,3,2000), "Foot", "Lille"));
 			// System.err.println(getMessage()) ;
 			// Question 3
-			System.out.println("Nombre de fois où il y le mot exposé dans le titre : "+agd.nbExpose());
+			System.out.println("Nombre de fois où il y le mot exposé dans le titre : "+agd.nbExpose("expose"));
 			// Question 4
 			System.out.println("Nombre de fois où la date correspond : "+agd.nbEvtEnregistre(new Date(2,1,1999)));
 			// Question 7
