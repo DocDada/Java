e8b2df033d35459e1ab80d2fd57fcb7e194004bf
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2018-03-15 17:56:56 +0100

    tp4 ihm - avant dernière question

---
+++ ihm/gridBagLayout/modele/Heure.java
@@ -0,0 +1,27 @@
+package modele;
+
+public class Heure {
+	private int heures ;
+	private int minutes ;
+	
+	public Heure(int parH, int parM) {
+		heures = parH;
+		minutes = parM ;
+	}
+
+	public int getHeures() {
+		return heures;
+	}
+
+	public void setHeures(int heures) {
+		this.heures = heures;
+	}
+
+	public int getMinutes() {
+		return minutes;
+	}
+
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
+}
