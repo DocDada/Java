ae2d0786847def5749a79a5ea8b5fd9dc7cbbc34
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2018-03-15 17:53:42 +0100

    tp4 ihm - dernière question

--- ihm/gridBagLayout/vue/PanelAgenda.java
+++ ihm/gridBagLayout/vue/PanelAgenda.java
@@ -1,27 +1,19 @@
 package vue;
 
-import javax.swing.JLabel ;
 import javax.swing.JPanel ;
-import javax.swing.JButton ;
-import javax.swing.ImageIcon ;
-
-import modele.Agenda;
+import modele.AgendaV2;
 import modele.Date;
 import modele.Evenement;
 
-import java.awt.CardLayout ;
-import java.awt.BorderLayout ;
-import java.awt.event.ActionListener ;
-import java.awt.event.ActionEvent ;
 
 
 public class PanelAgenda extends JPanel {
 	private static final long serialVersionUID = 1L;
-	Agenda aged = new Agenda(3);
+	AgendaV2 aged = new AgendaV2();
     PanelFormulaire panelF = new PanelFormulaire(aged) ;
     public PanelAgenda() {
-        //boolean truc = aged.ajout(new Evenement(new Date(1,1,1999), "controle", "g26"));
-        //boolean toto = aged.ajout(new Evenement(new Date(1,2,1999), "controle", "g26"));
+        aged.ajout(new Evenement(new Date(1,1,1999), "controle", "g26"));
+        aged.ajout(new Evenement(new Date(1,2,1999), "controle2", "g25"));
         this.add(panelF) ;
     }
 
