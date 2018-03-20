f8f9b64d5e3af600cefdaba08af242b26aa8053b
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2018-03-20 16:38:37 +0100

    tp6 ihm controleur

---
+++ ihm/gridBagLayout/controleur/Controleur.java
@@ -0,0 +1,37 @@
+package controleur ;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
+import modele.AgendaV2;
+import vue.PanelCalendrier;
+import vue.PanelFormulaire;
+
+public class Controleur implements ActionListener {
+	AgendaV2 agenda ;
+	PanelFormulaire panelForm ;
+	PanelCalendrier panelCale ;
+	
+	public Controleur(AgendaV2 agd, PanelFormulaire panelF, PanelCalendrier panelC) {
+		agenda = agd ;
+		panelForm = panelF ;
+		panelCale = panelC ;
+		panelForm.enregistreEcouteur(this) ;//le panel de saisie se met a l'ecoute
+		panelCale.enregistreEcouteur(this) ;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("+")) {
+			agenda.ajout(panelForm.getEvenement());
+			JOptionPane.showMessageDialog((JButton) e.getSource(), agenda.toString());
+			panelForm.reset() ;
+		}
+		
+	}
+	
+	
+}
