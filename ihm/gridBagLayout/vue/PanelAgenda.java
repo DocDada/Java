45f98778d04663719290d6ed0f1dca1749e47b0b
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2018-03-20 11:23:59 +0100

    tp5 ihm - déclaration d'un panelCalendrier (erreur)

--- ihm/gridBagLayout/vue/PanelAgenda.java
+++ ihm/gridBagLayout/vue/PanelAgenda.java
@@ -10,11 +10,13 @@ import modele.Evenement;
 public class PanelAgenda extends JPanel {
 	private static final long serialVersionUID = 1L;
 	AgendaV2 aged = new AgendaV2();
+	PanelCalendrier panelC = new PanelCalendrier();
     PanelFormulaire panelF = new PanelFormulaire(aged) ;
     public PanelAgenda() {
         aged.ajout(new Evenement(new Date(1,1,1999), "controle", "g26"));
         aged.ajout(new Evenement(new Date(1,2,1999), "controle2", "g25"));
         this.add(panelF) ;
+        this.add(panelC) ;
     }
 
 
