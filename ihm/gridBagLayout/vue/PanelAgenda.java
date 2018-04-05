83869bad837ed351c5a5cff13abe5e275ded43c9
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2018-04-05 17:55:56 +0200

    tp6 java lecture fichier

--- ihm/gridBagLayout/vue/PanelAgenda.java
+++ ihm/gridBagLayout/vue/PanelAgenda.java
@@ -1,10 +1,12 @@
 package vue;
 
+import java.io.File;
 import javax.swing.JPanel ;
 import controleur.Controleur;
 import modele.AgendaV2;
 import modele.Date;
 import modele.Evenement;
+import modele.LectureEcriture;
 
 
 
@@ -15,11 +17,20 @@ public class PanelAgenda extends JPanel {
 	PanelCalendrier panelC = new PanelCalendrier();
     PanelFormulaire panelF = new PanelFormulaire() ;
     Controleur ctrl = new Controleur(aged, panelF, panelC);
-
+    File fichier = new File("FileAgenda"+File.separator+"agendas.ser");
+    
+    	
+    
     public PanelAgenda() {
-        aged.ajout(new Evenement(new Date(1,1,1999), "controle", "g26"));
-        aged.ajout(new Evenement(new Date(1,2,1999), "controle2", "g25"));
-        
+    	if (fichier.length()!=0 && fichier.isFile()) {
+	    	aged.ajout(new Evenement(new Date(1,1,1999), "controle", "g26"));
+	        aged.ajout(new Evenement(new Date(1,2,1999), "controle2", "g25"));
+    	}
+    	else {
+    		aged.ajout((Evenement)LectureEcriture.lecture(fichier));
+    	}
+    
+
         this.add(panelC) ;// calendrier a gauche
         this.add(panelF) ;// formulaire a droite
     }// PanelAgenda()
