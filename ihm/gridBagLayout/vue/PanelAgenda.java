5aa60c57278fbedb5ef1a974c05e5301cdfb6437
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2018-04-09 18:42:36 +0200

    tp6 java fini (lecture/écriture de fichiers)
    ajout événement à l'agenda => écriture d'un !!!! événement !!!!
    lance l'application => lecture fichier

--- ihm/gridBagLayout/vue/PanelAgenda.java
+++ ihm/gridBagLayout/vue/PanelAgenda.java
@@ -4,7 +4,6 @@ import java.io.File;
 import javax.swing.JPanel ;
 import controleur.Controleur;
 import modele.AgendaV2;
-import modele.Date;
 import modele.Evenement;
 import modele.LectureEcriture;
 
@@ -22,14 +21,8 @@ public class PanelAgenda extends JPanel {
     	
     
     public PanelAgenda() {
-    	if (fichier.length()!=0 && fichier.isFile()) {
-	    	aged.ajout(new Evenement(new Date(1,1,1999), "controle", "g26"));
-	        aged.ajout(new Evenement(new Date(1,2,1999), "controle2", "g25"));
-    	}
-    	else {
-    		aged.ajout((Evenement)LectureEcriture.lecture(fichier));
-    	}
-    
+    	if (fichier.length()!=0)
+	        aged.ajout((Evenement)LectureEcriture.lecture(fichier));
 
         this.add(panelC) ;// calendrier a gauche
         this.add(panelF) ;// formulaire a droite
