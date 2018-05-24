decd42e3889f0a38398c7e0a63a617ab89af3492
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2018-05-24 18:02:14 +0200

    javadoc

--- poo/tp2matrice/Matrix.java
+++ poo/tp2matrice/Matrix.java
@@ -3,13 +3,58 @@ package tp2matrice;
 import java.text.DecimalFormat;
 
 /**
+ * Tableau de double. Peut être un tableau à double entrée, ou bien unidimensionnel.<br>
+ * En mathématiques, les matrices sont des tableaux de nombres 
+ * qui servent à interpréter en termes calculatoires et donc opérationnels 
+ * les résultats théoriques de l'algèbre linéaire et même 
+ * de l'algèbre bilinéaire. Toutes les disciplines étudiant des 
+ * phénomènes linéaires utilisent les matrices. Quant 
+ * aux phénomènes non linéaires, on en donne souvent des approximations 
+ * linéaires, comme en optique géométrique avec les approximations de <b>Gauss</b>.<br>
+ * 
+ * Exemple de matrice <b>carré</b> :<br>
+ * 
+ *     [0, 1, 5]<br>
+ * C = [4, 8, 9]<br>
+ *     [7, 9, 3]<br>
+ * 
+ * Exemple de matrice <b>ligne</b> :<br>
+ * 
+ * L = [0, 1, 5, 6, 8]<br>
+ * 
+ * Exemple de matrice <b>colonne</b> :<br>
+ * 
+ *     [9]<br>
+ * C = [3]<br>
+ *     [4]<br>
+ * 
+ * Exemple de matrice <b>symétrique</b> :<br>
+ * 
+ *     [0, 4, 5]<br>
+ * S = [4, 8, 9]<br>
+ *     [5, 9, 3]<br>
+ * 
+ * Exemple de matrice <b>identité</b> :<br>
+ * 
+ *     [1, 0, 0]<br>
+ * I = [0, 1, 0]<br>
+ *     [0, 0, 1]<br>
+ * 
+ * 
  * 
  * @author prepain
+ * @version 1
  *
  */
 public abstract class Matrix {
 
+    /**
+     * Nombre de lignes.
+     */
     protected int nRow;
+    /**
+     * Nombre de colonnes.
+     */
     protected int nCol;
 
     ///////////////////////
@@ -19,7 +64,7 @@ public abstract class Matrix {
     ///////////////////////
 
     /**
-     * Construit une matrice avec un nombre de lignes et de colonnes nul
+     * Construit une matrice avec un nombre de lignes et de colonnes nuls.
      */
     protected Matrix() {
         nRow = 0;
@@ -27,7 +72,7 @@ public abstract class Matrix {
     }
 
     /**
-     * Construit une matrice ayant pour dimensions n lignes et m colonnes
+     * Construit une matrice ayant pour dimensions n lignes et m colonnes.
      * @param n nombre de lignes
      * @param m nombre de colonnes
      */
@@ -44,16 +89,18 @@ public abstract class Matrix {
 
     /**
      * Affiche la matrice dans la console
+     * @see Matrix#get(int, int)
      */
     public void write() {
         DecimalFormat formatEnDecimales = new DecimalFormat("0.00");
         for (int lig = 0; lig < nRow; lig++)
             for (int col = 0; col < nCol; col++)
-                System.out.print((col == 0 ? "\n[" : "") + formatEnDecimales.format(get(lig, col)) + (col == nCol - 1 ? "]" : ", "));
+                System.out.print((col == 0 ? "\n[" : "") + formatEnDecimales.format(this.get(lig, col)) + (col == nCol - 1 ? "]" : ", "));
     }// write()
 
     /**
      * Affecte des valeurs générées aléatoirement à la matrice
+     * @see Matrix#set(int, int, double)
      */
     public void random() {
         for (int lig = 0; lig < nRow; lig++)
@@ -62,9 +109,14 @@ public abstract class Matrix {
     }// random()
 
     /**
+     * Effectue la somme de la matrice B et de la matrice courante
      * @param B Matrice qui sera ajouté à la matrice courante
-     * @return La somme de la matrice B et de la matrice courante
+     * @return la somme de matrice courante et de la matrice B.
      * @throws ExceptionMatrix les deux matrices doivent être de mêmes dimensions
+     * @see Matrix#memeDimension(Matrix)
+     * @see Matrix#copy()
+     * @see Matrix#get(int, int)
+     * @see Matrix#set(int, int, double)
      */
     public Matrix sum(Matrix B) throws ExceptionMatrix {
         if (!this.memeDimension(B))
@@ -77,9 +129,14 @@ public abstract class Matrix {
     }// sum()
 
     /**
+     * Effectue la différence de la matrice courante et de la matrice B
      * @param B Matrice qui sera soustrait à la matrice courante
-     * @return La différence de la matrice courante et de la matrice B
+     * @return la différence de la matrice courante et de la matrice B
      * @throws ExceptionMatrix les deux matrices doivent être de mêmes dimensions
+     * @see Matrix#memeDimension(Matrix)
+     * @see Matrix#copy()
+     * @see Matrix#get(int, int)
+     * @see Matrix#set(int, int, double)
      */
     public Matrix minus(Matrix B) throws ExceptionMatrix {
         if (!this.memeDimension(B))
@@ -94,7 +151,10 @@ public abstract class Matrix {
     /**
      * Multiplie la matrice this par un scalaire s (s étant un réel)
      * @param s le facteur multiplicateur
-     * @return Le résultat de la multiplication de la matrice par le scalaire
+     * @return le résultat de la multiplication de la matrice par le scalaire
+     * @see Matrix#copy()
+     * @see Matrix#get(int, int)
+     * @see Matrix#set(int, int, double)
      */
     public Matrix mult(double s) {
         Matrix matrice = this.copy();
@@ -105,6 +165,7 @@ public abstract class Matrix {
     }// mult()
 
     /**
+     * Effectue le produit de la matrice courante et de la matrice B
      * @param B Matrice qui sera multiplié à la matrice courante
      * @return le produit de la matrice courante et de la matrice B
      * @throws ExceptionMatrix La matrice courante doit avoir autant de colonnes que B a de lignes
@@ -113,6 +174,7 @@ public abstract class Matrix {
 
     /**
      * Met à zéros tous les coefficients de la matrice courante
+     * @see Matrix#set(int, int, double)
      */
     public void zeros() {
         for (int lig = 0; lig < nRow; lig++)
@@ -123,6 +185,8 @@ public abstract class Matrix {
     /**
      * Affecte l'identité à la matrice courante
      * @throws ExceptionMatrix la matrice doit être une matrice carré
+     * @see Matrix#matriceCarre()
+     * @see Matrix#zeros()
      */
     public void identity() throws ExceptionMatrix {
         if (!this.matriceCarre())
@@ -139,7 +203,8 @@ public abstract class Matrix {
     public abstract Matrix copy();
 
     /**
-     * @return La transposé de la matrice courante
+     * Efectue la transposé de la matrice courante
+     * @return la transposé de la matrice courante
      */
     public abstract Matrix transpose();
 
@@ -147,6 +212,8 @@ public abstract class Matrix {
      * Retourne un booléen selon si la matrice courante et la matrice B ont les mêmes dimensions
      * @param B Matrice B
      * @return true si les matrices ont les mêmes dimensions, false sinon
+     * @see Matrix#getColDimension()
+     * @see Matrix#getRowDimension()
      */
     public boolean memeDimension(Matrix B) {
         return B.getColDimension() == this.nCol && B.getRowDimension() == this.nRow;
@@ -162,12 +229,17 @@ public abstract class Matrix {
     /**
      * @param i Indice de la ligne
      * @param j Indice de la colonne
-     * @return L'indice pour un tableau à une dimension
+     * @return l'indice pour un tableau à une dimension
      */
     public int index(int i, int j) {
         return i * this.nCol + j;
     }
     
+    /**
+     * Renvoie un tableau d'index à partir d'un indice d'un tableau unidimensionnel
+     * @param ij l'indice d'un tableau unidimensionnel
+     * @return un tableau d'indices
+     */
     public int[] indexInv(int ij) {
         return new int[] {ij/nCol, ij%nCol};
     }
@@ -181,14 +253,16 @@ public abstract class Matrix {
     //////////////////////
 
     /**
-     * @return Le nombre de lignes de la matrice courante
+     * Renvoie le nombre de lignes de la matrice courante
+     * @return le nombre de lignes de la matrice courante
      */
     public int getRowDimension() {
         return this.nRow;
     }
 
     /**
-     * @return Le nombre de colonnes de la matrice courante
+     * Renvoie le nombre de colonnes de la matrice courante
+     * @return le nombre de colonnes de la matrice courante
      */
     public int getColDimension() {
         return this.nCol;
@@ -196,7 +270,7 @@ public abstract class Matrix {
 
     /**
      * Modifie le nombre de lignes de la matrice courante
-     * @param nRow Nombre de lignes
+     * @param nRow nombre de lignes
      */
     public void setRowDimension(int nRow) {
         this.nRow = nRow;
@@ -228,24 +302,39 @@ public abstract class Matrix {
     }// set()
 
     /**
+     * Renvoie la valeur se trouvant à l'indice (i, j)
      * @param i indice de la ligne
      * @param j indice de la colonne
-     * @return La valeur se trouvant à l'indice (i, j)
+     * @return la valeur se trouvant à l'indice (i, j)
      */
     public abstract double get(int i, int j);
 
+    /**
+     * Renvoie la valeur se trouvant à l'indice (ij)
+     * @param ij l'indice
+     * @return la valeur se trouvant à l'indice (ij)
+     * @see Matrix#indexInv(int)
+     */
     public abstract double get(int ij);
     
     /**
+     * Modifie la valeur à l'indice (i, j)
      * @param i indice de la ligne
      * @param j indice de la colonne
-     * @param aij La valeur à placer à l'indice (i, j)
+     * @param aij la valeur à placer à l'indice (i, j)
      */
     public abstract void set(int i, int j, double aij);
 
+    /**
+     * Modifie la valeur à l'indice (ij)
+     * @param ij l'indice
+     * @param aij la valeur à placer à l'indice (ij)
+     * @see Matrix#indexInv(int)
+     */
     public abstract void set(int ij, double aij);
     
-    /** retourne une sous matrice telle que : soient A, B (de dimensions nRow et nCol; nRow et nCol étant réels), B = A(iStart : iEnd, jStart : jEnd)
+    /**
+     * Retourne une sous matrice telle que : soient A, B (de dimensions nRow et nCol; nRow et nCol étant réels), B = A(iStart : iEnd, jStart : jEnd)
      * @param iStart l'indice de la ligne (début du tronçon)
      * @param iEnd l'indice de la ligne (fin du tronçon)
      * @param jStart l'indice de la colonne (début du tronçon)
